653310e841ff0500e06191a1330964e107e599dd
Teona46 <teonika2406@gmail.com>
2024-10-04 00:10:03 +0300

    Homework-11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    static void task1() {
+        System.out.println("Task 1");
+        System.out.println(Methods.square(6));
+        System.out.println(Methods.square(33));
+        System.out.println(Methods.square(4));
+        System.out.println(Methods.square(56));
+        System.out.println();
+    }
+    static void task2() {
+        System.out.println("Task 2");
+        System.out.println(Methods.printFullName("Teona", "Chelidze"));
+        System.out.println();
+    }
+    static void task3() {
+        System.out.println("Task 3");
+        String [] words = {"Yesterday", "love", "was", "such", "an", "easy", "game", "to", "play"};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(words)));
+
+
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,3 +1,23 @@
 public class Methods {
+    static int square(int number) {
+        int numSquare = (int) Math.pow(number, 2);
+        return numSquare;
+    }
+    static String printFullName(String name, String lastName) {
+        return name + " " + lastName;
+    }
 
+    static String[] cornerValuesArray(String[] array) {
+        String shortText = array[0];
+        String longText = array[0];
+        for (String element : array) {
+            if (element.length() < shortText.length()) {
+                shortText = element;
+            }
+            if (element.length() > longText.length()) {
+                longText = element;
+            }
+        }
+        return new String[]{shortText, longText};
+    }
 }
